f2bf9ad34989b5dad41b352d10f4ae6f554f01fd
siam <siamtalukder135@gamil.com>
2024-01-19 19:24:33 +0600

    last

--- register-api.php
+++ register-api.php
@@ -64,7 +64,7 @@ if ($duplicate_result->num_rows > 0) {
     $ans=-1;
 } 
 else if($individualCharacters != $id){
-    echo json_encode(array('message' => 'ID Or Email not match ' ,'status' => false));
+    echo json_encode(array('message' => 'ID AND EMAIL NOT MATCH ' ,'status' => false));
 }
 
 //$sql = "INSERT INTO student (name,id,email) VALUES ('$name','$id','$email')";
