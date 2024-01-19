9d15f02d19fcfb5fb47149b4d2932e0135ec5c59
siam <siamtalukder135@gamil.com>
2024-01-19 14:54:44 +0600

    login done

--- login.php
+++ login.php
@@ -3,30 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login page</title>
+    <title>Login Page</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body id="bodyy">
     <center>
-    <form action="#" method="POST" autocomplete="off">
-       <h1>WELCOME FOR LOGIN</h1>
-        <table>
-            <tr>
-                <td><b>Email</b></td>
-                <td><input type="email" id="email" name="email"></td>
-            </tr>
-            <tr>
-                <td><b>Password</b></td>
-                <td><input type="password" id="password" name="password"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="login" id="login" value="Login"></td>
-            </tr>
-        </table>
-        <p>not register? <a href="register.html">Register</a></p>
-    </form>
-</center>
+        <form action="#" method="POST" autocomplete="off">
+            <h1>WELCOME FOR LOGIN</h1>
+            <table>
+                <tr>
+                    <td><b>Email</b></td>
+                    <td><input type="email" id="email" name="email" required></td>
+                </tr>
+                <tr>
+                    <td><b>Password</b></td>
+                    <td><input type="password" id="password" name="password" required></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="login" id="login" value="Login"></td>
+                </tr>
+            </table>
+            <p>Not registered? <a href="register.html">Register</a></p>
+        </form>
+    </center>
 </body>
 </html>
 
@@ -38,16 +38,24 @@ if ($conn->connect_error) {
 }
 
 if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $query = "SELECT * FROM mbstublood WHERE email='$email' AND password='$password'";
-    $data = mysqli_query($conn, $query);
-    $total = mysqli_num_rows($data);
-    if($total==1){
-        header('location:blood group name page.php');
-    }
-    else {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);  // Use mysqli_real_escape_string to prevent SQL injection
+    $password = mysqli_real_escape_string($conn, $_POST['password']);  // Use mysqli_real_escape_string to prevent SQL injection
 
+    $query = "SELECT * FROM mbstublood WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        die("Query failed: " . mysqli_error($conn));
+    }
+
+    $total = mysqli_num_rows($result);
+
+    if ($total == 1) {
+        header('location: blood group name page.php');
+        exit;  // It's a good practice to exit after a header redirect
+    } else {
+    
+        echo "Invalid email or password";  // You can provide a message or redirect to a login error page
     }
 }
 ?>
